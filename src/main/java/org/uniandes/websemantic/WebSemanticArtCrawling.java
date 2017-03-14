2f96a5e97da01bc73084f9ffe392e33e19ffa2dd
Daniel Althviz Moré <dalthviz@users.noreply.github.com>
2017-03-13 20:41:36 -0500

    Adiciona artcyclopedia

--- src/main/java/org/uniandes/websemantic/WebSemanticArtCrawling.java
+++ src/main/java/org/uniandes/websemantic/WebSemanticArtCrawling.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 
 import org.uniandes.websemantic.page.Artnet;
 import org.uniandes.websemantic.page.Wga;
+import org.uniandes.websemantic.page.Artcyclopedia;
 
 public class WebSemanticArtCrawling {
 
 	public static void main(String[] args) {
 		Artnet.crawling();
+		Artcyclopedia.crawling();
 		Wga wga = new Wga();
 		wga.crawling();
 		WikiArt.crawling();
